15007d21d54e9b86bca9a1451b017684dd1debd0
Davi Vieira <145080064+davieir4@users.noreply.github.com>
2024-12-18 12:39:35 -0300

    Create index.php
    mano deivyn

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img  src=https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/d4f5d715-91d2-45b0-b397-06ee11da4277/width=450/FLUX_GGUF_Q8_00537_.jpeg>
+    <H1>
+        Mano Deyvin sound effects
+        :
+        
+    </H1>
+</body>
+</html>
+
+<?php
+
+for($i = 0; $i < 3; $i++){
+
+echo "ai <br>";
+
+}
+
+?>
